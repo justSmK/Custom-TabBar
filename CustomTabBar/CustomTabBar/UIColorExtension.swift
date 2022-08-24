1abc32f74574b5ca4a5000254bf1543f69984b8a
justSmK <semko431@gmail.com>
2022-08-24 13:41:17 +0300

    Added MainTabBarController with view controllers

---
+++ CustomTabBar/CustomTabBar/UIColorExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UIColorExtension.swift
+//  CustomTabBar
+//
+//  Created by justSmK on 8/24/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var tabBarItemAccent: UIColor {
+        #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+    }
+    
+    static var mainWhite: UIColor {
+        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    static var tabBarItemLight: UIColor {
+        #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+    }
+}
